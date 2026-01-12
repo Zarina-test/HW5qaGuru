0874fa6bdaf7d3ffcf91c2578a846cc7d8cdfe70
Zarina-test <zarina@somebugs.com>
2026-01-12 13:53:35 +0300

    Homework

---
+++ src/helpers/pages/article.page.js
@@ -0,0 +1,28 @@
+export class ArticlePage {
+
+    constructor(page) {
+        
+        this.page = page;
+        this.articleTitle = page.locator('h1');
+        this.articleBody = page.locator('.article-content p');
+        this.articleTags = page.locator('.tag-list .tag-pill');
+        this.commentInput = page.getByPlaceholder('Write a comment...');
+        this.postCommentButton = page.getByRole('button', { name: 'Post Comment' });
+        this.comments = page.locator('.card .card-block p');
+        this.favoriteButton = page.getByRole('button', { name: 'Favorite Article' });
+        this.followButton = page.getByRole('button', { name: 'Follow' });
+    }
+
+    async getArticleTitle() {
+        return await this.articleTitle.textContent();
+    }
+
+    async addComment(comment) {
+        await this.commentInput.fill(comment);
+        await this.postCommentButton.click();
+    }
+
+    async getComments() {
+        return await this.comments.allTextContents();
+    }
+};
---
+++ src/helpers/pages/new-article.page.js
@@ -0,0 +1,23 @@
+export class NewArticlePage {
+
+    constructor(page) {
+
+        this.page = page;
+        this.titleInput = page.getByPlaceholder('Article Title');
+        this.descriptionInput = page.getByPlaceholder("What's this article about?");
+        this.bodyInput = page.getByPlaceholder('Write your article (in markdown)');
+        this.tagsInput = page.getByPlaceholder('Enter tags');
+        this.publishButton = page.getByRole('button', { name: 'Publish Article' });
+    }
+
+    async createArticle(title, description, body, tags = []) {
+        
+        await this.titleInput.fill(title);
+        await this.descriptionInput.fill(description);
+        await this.bodyInput.fill(body);
+        if (tags.length > 0) {
+            await this.tagsInput.fill(tags.join(' '));
+        }
+        await this.publishButton.click();
+    }
+};
